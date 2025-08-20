7ec9d8a7cb725840ff59b9bcf0b5cf5fc34d6672
Thalys <thalysvinicius23@gmail.com>
2025-08-19 21:54:36 -0300

    Add initial JavaScript functionality for Guess My Number game

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+document.querySelector('.check').addEventListener('click', function () {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess);
+    if (!guess) {
+        document.querySelector('.message').textContent = 'No number!';
+    }
+})
